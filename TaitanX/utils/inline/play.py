f9ccc055a4a6b02805b1832f460132aee293b01e
Tait3nX <150482325+Tait3nX@users.noreply.github.com>
2023-11-11 19:23:41 +0530

    Update play.py

--- TaitanX/utils/inline/play.py
+++ TaitanX/utils/inline/play.py
@@ -27,46 +27,48 @@ def track_markup(_, videoid, user_id, channel, fplay):
     return buttons
 
 
-def stream_markup_timer(_, chat_id, played, dur):
+def stream_markup_timer(_, videoid, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
-    umm = math.floor(percentage)
-    if 0 < umm <= 10:
-        bar = "◉—————————"
-    elif 10 < umm < 20:
-        bar = "—◉————————"
-    elif 20 <= umm < 30:
-        bar = "——◉———————"
-    elif 30 <= umm < 40:
-        bar = "———◉——————"
-    elif 40 <= umm < 50:
-        bar = "————◉—————"
-    elif 50 <= umm < 60:
-        bar = "—————◉————"
-    elif 60 <= umm < 70:
-        bar = "——————◉———"
-    elif 70 <= umm < 80:
-        bar = "———————◉——"
-    elif 80 <= umm < 95:
-        bar = "————————◉—"
+    anon = math.floor(percentage)
+    if 0 < TaitanX <= 10:
+        bar = "⚪─────────"
+    elif 10 < TaitanX < 20:
+        bar = "━⚪────────"
+    elif 20 <= TaitanX < 30:
+        bar = "━━⚪───────"
+    elif 30 <= TaitanX < 40:
+        bar = "━━━⚪──────"
+    elif 40 <= TaitanX < 50:
+        bar = "━━━━⚪─────"
+    elif 50 <= TaitanX < 60:
+        bar = "━━━━━⚪────"
+    elif 60 <= TaitanX < 70:
+        bar = "━━━━━━⚪───"
+    elif 70 <= TaitanX < 80:
+        bar = "━━━━━━━⚪──"
+    elif 80 <= TaitanX < 95:
+        bar = "━━━━━━━━⚪─"
     else:
-        bar = "—————————◉"
+        bar = "━━━━━━━━━⚪"
+
     buttons = [
-        [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
         [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
         ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        [
+            InlineKeyboardButton(text="𝗦𝘂𝗽𝗽𝗼𝗿𝘁 💭", url=f"t.me/TaitanXSupport"),
+            InlineKeyboardButton(text="𝗨𝗽𝗱𝗮𝘁𝗲𝘀 💬", url=f"t.me/TaitanXBot"),
+            )
+        ],
+        [
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")
+            )
+        ],
     ]
     return buttons
 
@@ -74,13 +76,20 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            )
         ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        [
+            InlineKeyboardButton(text="𝗦𝘂𝗽𝗽𝗼𝗿𝘁 💭", url=f"t.me/TaitanXSupport"),
+            InlineKeyboardButton(text="𝗨𝗽𝗱𝗮𝘁𝗲𝘀 💬", url=f"t.me/TaitanXBot"),
+            )
+        ],
+        [
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")
+            )
+        ],
     ]
     return buttons
 
@@ -89,19 +98,18 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["P_B_1"],
-                callback_data=f"DAXXPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
-            ),
-            InlineKeyboardButton(
-                text=_["P_B_2"],
-                callback_data=f"DAXXPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
-            ),
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            )
         ],
         [
-            InlineKeyboardButton(
-                text=_["CLOSE_BUTTON"],
-                callback_data=f"forceclose {videoid}|{user_id}",
-            ),
+            InlineKeyboardButton(text="𝗦𝘂𝗽𝗽𝗼𝗿𝘁 💭", url=f"t.me/TaitanXSupport"),
+            InlineKeyboardButton(text="𝗨𝗽𝗱𝗮𝘁𝗲𝘀 💬", url=f"t.me/TaitanXBot"),
+            )
+        ],
+        [
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")
+            )
         ],
     ]
     return buttons
