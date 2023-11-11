365c6de5c211223d70aef4eef158ac74c41f3f04
Tait3nX <150482325+Tait3nX@users.noreply.github.com>
2023-11-11 08:14:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/Tait3nX/TaitanXMusic",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
