6e5da6dee7ab1b240790d041c07e971b6177065b
instgram8736382 <146512731+instgram8736382@users.noreply.github.com>
2023-11-13 20:05:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Tait3nX/TaitanXMusic",
+    "https://github.com/instgram8736382/TaitanXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
